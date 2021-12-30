0b112f952095d56789cdd7448b31c91ecd0baf58
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2021-12-30 15:21:06 -0300

    Enviando o primeiro requisito

---
+++ script.js
@@ -0,0 +1,47 @@
+const corpo = document.getElementById('body');
+const cabecalho = document.getElementById('header');
+const titulo = document.getElementById('title');
+const footer = document.getElementById('footer');
+const rodaPe = document.getElementById('rodape');
+const luz = document.getElementById('light');
+const luzes = document.getElementById('label');
+
+const darkJungleGreen = 'rgba(0, 21, 20, 1)';
+const white = 'rgba(255, 255, 255, 1)';
+const bloodRed = 'rgba(107, 5, 4, 1)';
+const chineseRed = 'rgba(163, 50, 11, 1)';
+const goldenRod = 'rgba(230, 175, 46, 1)';
+
+function ascende() {
+  corpo.style.backgroundColor = white;
+  cabecalho.style.backgroundColor = goldenRod;
+  cabecalho.style.borderColor = darkJungleGreen;
+  footer.style.backgroundColor = goldenRod;
+  footer.style.borderColor = darkJungleGreen;
+  titulo.style.color = chineseRed;
+  titulo.style.textShadow = `2px 2px 2px ${bloodRed}`;
+  rodaPe.style.textShadow = `2px 2px 2px ${bloodRed}`;
+  luzes.style.color = 'black';
+}
+
+function apaga() {
+  corpo.style.backgroundColor = darkJungleGreen;
+  cabecalho.style.backgroundColor = darkJungleGreen;
+  cabecalho.style.borderColor = chineseRed;
+  footer.style.backgroundColor = darkJungleGreen;
+  footer.style.borderColor = chineseRed;
+  titulo.style.color = bloodRed;
+  titulo.style.textShadow = `2px 2px 2px ${white}`;
+  rodaPe.style.textShadow = `2px 2px 2px ${goldenRod}`;
+  luzes.style.color = white;
+}
+
+function ascenderLuz() {
+  if (luz.checked === true) {
+    ascende();
+  } else {
+    apaga();
+  }
+}
+
+luz.addEventListener('change', ascenderLuz);
